4d9a4e67abe770c99e57ab0c370403d8270200c3
AUcod3r <webmaster@code320.com>
2020-02-26 11:03:23 -0600

    Updated and refactored

--- dataUsage.py
+++ dataUsage.py
@@ -14,9 +14,7 @@ def days_between(startDate, tnow):
 
     startDate is a constant. tnow is today.
     """
-    startDate = datetime.datetime.strptime(startDate, "%Y-%m-%d")
-    tnow = datetime.datetime.strptime(tnow, "%Y-%m-%d")
-    return abs((tnow - startDate).days)
+    return (tnow - startDate).days
 
 
 def readData():
@@ -32,9 +30,8 @@ def readData():
         if line == '\n':
             return 0
         else:
-            last = line.strip()[40:]
-            dataYesterday = last.strip()
-            return dataYesterday
+            last = line.split(' ')
+            return last[-1].strip()
 
 
 def writeData():
@@ -44,24 +41,24 @@ def writeData():
     startDate is a constant. Is today.
     """
     with open('dataUsed.txt', 'a') as f:
-        f.write(tnow + ' - ' + "Data used: " + str(dataUsed) +
+        f.write(str(tnow) + ' - ' + "Data used: " + str(dataUsed) +
                 ' - ' + "Data left: " + str(dataLeft) + '\n')
 
 
 startDate = str(start)
-tnow = str(datetime.date.today())
+tnow = datetime.date.today()
 endDate = str(end)
 lastDay = end + datetime.timedelta(days=1)
 cStart = c.colors.bold + c.colors.bg.black + c.colors.fg.yellow
 cEnd = c.colors.reset
-totalNumDays = days_between(startDate, endDate)
-numDays = days_between(startDate, tnow)
-daysLeft = days_between(tnow, endDate)
+totalNumDays = days_between(start, end)
+numDays = days_between(start, tnow)
+daysLeft = days_between(tnow, end)
 
 os.system("clear")
 dataUsed = int(input('Provide the data used thusfar in G-bytes: '))
-dataLeft = 1024 - dataUsed
-perCentUsed = (dataUsed / 1024) * 100
+dataLeft = 1500 - dataUsed
+perCentUsed = (dataUsed / 1500) * 100
 
 print('Today is: ', tnow, '\nStart date is: ', startDate,
       '\nEnd Date is: ', endDate, '\nTotal days in plan: ', cStart,
@@ -69,21 +66,20 @@ print('Today is: ', tnow, '\nStart date is: ', startDate,
 if numDays > totalNumDays:
     numDays = totalNumDays
 print('Number of days since start: ', cStart, numDays, cEnd)
-print('Days left in your plan is: ', cStart, daysLeft, cEnd)
-print('Percentage of time since start: ', cStart,
-      round((numDays * 100 / totalNumDays), 2), cEnd)
-print('Percentage of 1024 GBytes used is: ', cStart, round(perCentUsed, 2), cEnd)
-print('Your average usage per day is: ', cStart,
-      round(dataUsed / numDays, 2), cEnd, 'Gigabytes')
-print('Amount of data left is: ', cStart, dataLeft, cEnd, 'Gigabytes')
 if daysLeft >= 0:
+    print('Days left in your plan is: ', cStart, daysLeft, cEnd)
+    print('Percentage of time since start: ', cStart,
+          round((numDays * 100 / totalNumDays), 2), cEnd)
+    print('Percentage of 1500 GBytes used is: ', cStart, round(perCentUsed, 2), cEnd)
+    print('Your average usage per day is: ', cStart,
+          round(dataUsed / numDays, 2), cEnd, 'Gigabytes')
+    print('Amount of data left is: ', cStart, dataLeft, cEnd, 'Gigabytes')
     print('You need to stay under: ', cStart, round(dataLeft / daysLeft, 2), cEnd,
           'Gigabytes per day to avoid additional cost\n\n')
 else:
     print('Yesterday was the last day of the plan!!')
 
 dataYesterday = readData()
-
 if dataYesterday is None:
     dataYesterday = 0
     dataUsedYesterday = dataUsed
@@ -98,5 +94,5 @@ writeData()
 print('\n\n**************************\n\n')
 os.system("tail -n 5 dataUsed.txt")
 
-if tnow == str(lastDay):
+if tnow == lastDay:
     os.system(f"mv dataUsed.txt {endDate}.txt && touch dataUsed.txt")
